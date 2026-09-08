fdfa742276b7e94fd74429b0b4d9b432d458731f
Walker Leite <walkerleite490@gmail.com>
2026-09-08 17:48:18 -0300

    chore(repo): keep tooling metadata out of root

--- devenv.nix
+++ devenv.nix
@@ -16,13 +16,12 @@
   tasks."govenv:docs".exec = ''
     rm -rf .docs-build _site
     mkdir -p .docs-build _site
-    agda -i . -i src --html --html-highlight=auto --css=site.css --html-dir=.docs-build Govenv.lagda.md
+    agda -i . -i src --html --html-highlight=auto --html-dir=.docs-build Govenv.lagda.md
     cp .docs-build/*.html _site/
-    cp "$(agda --print-agda-data-dir)/html/Agda.css" _site/Agda.css
-    cp docs/site.css _site/site.css
+    cp .docs-build/Agda.css _site/Agda.css
     for source in .docs-build/*.md; do
       name="$(basename "$source" .md)"
-      pandoc "$source" --standalone --metadata pagetitle="$name" --css=site.css -o "_site/$name.html"
+      pandoc "$source" --standalone --metadata pagetitle="$name" --css=Agda.css -o "_site/$name.html"
     done
     cp _site/Govenv.html _site/index.html
     touch _site/.nojekyll
